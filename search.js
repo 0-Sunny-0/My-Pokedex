e9e9832eed4cc503a76cb8119a74bf2ea481f4f5
Sunny Le Bunz <msafinch@gmail.com>
2024-08-10 10:43:51 -0400

    recoded pokemon.js for display and fetch errors, also fixed CSS

---
+++ search.js
@@ -0,0 +1,4 @@
+const inputElement = document.querySelector("#search-input");
+const search_icon = document.querySelector("#search-close-icon");
+const sort_wrapper = document.querySelector(".sort-wrapper");
+
